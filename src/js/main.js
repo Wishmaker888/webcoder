375ff32d9c3f25e1d05aa9d33e7c84ca6e428f47
Wishmaker888 <skywalker855@gmail.com>
2017-05-09 00:18:21 +0300

    Done with content text

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
  */
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/owl.carousel/dist/owl.carousel.js
+//= ../../bower_components/tether/dist/js/tether.js
+//= ../../bower_components/tether-drop/dist/js/drop.js
+//= ../../bower_components/tether-tooltip/dist/js/tooltip.js
 
 'use script';
 
